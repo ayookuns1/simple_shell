eddbb7a333948fac27679e9ac8e4dc8ccda63b98
ayookuns1 <ayookuns830@gmail.com>
2023-08-23 04:03:59 -0700

    update5

--- execute_command.c
+++ execute_command.c
@@ -7,36 +7,27 @@
  *
  * Return: 1 (On success), 0 (command not found)
  */
-int execute_command_with_path(char **args)
-{
-pid_t pid;
-int status;
+int execute_command_with_path(char **args) {
+    pid_t pid;
+    int status;
 
-char *command_path = find_command_in_path(args[0]);
-if (command_path == NULL)
-{
-return (0);
-}
+    char *command_path = find_command_in_path(args[0]);
+    if (command_path == NULL) {
+        return 0;
+    }
 
-pid = fork();
-if (pid == 0)
-{
-if (execvp(command_path, args) == -1)
-{
-perror("simple_shell");
-}
-exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-{
-perror("simple_shell");
-}
-else
-{
-wait(&status);
-}
+    pid = fork();
+    if (pid == 0) {
+        if (execvp(command_path, args) == -1) {
+            perror("simple_shell");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("simple_shell");
+    } else {
+        wait(&status);
+    }
 
-free(command_path);
-return (1);
+    free(command_path);
+    return 1;
 }
-
--- parse_input.c
+++ parse_input.c
@@ -8,41 +8,36 @@
  * Return: An arr of pointrs to the tokens parsed from the input str
  *
  */
-char **parse_input(char *input)
-{
-const char *delimiters = " \t\r\n\a";
-char *token;
-int bufsize = TOKEN_BUFSIZE;
-int position = 0;
+char **parse_input(char *input) {
+    const char *delimiters = " \t\r\n\a";
+    char *token;
+    int bufsize = TOKEN_BUFSIZE;
+    int position = 0;
 
-char **tokens = malloc(TOKEN_BUFSIZE * sizeof(char *));
-if (tokens == NULL)
-{
-perror("allocation error");
-exit(EXIT_FAILURE);
-}
+    char **tokens = malloc(TOKEN_BUFSIZE * sizeof(char *));
+    if (tokens == NULL) {
+        perror("allocation error");
+        exit(EXIT_FAILURE);
+    }
 
-token = strtok(input, delimiters);
+    token = strtok(input, delimiters);
 
-while (token != NULL)
-{
-tokens[position] = token;
-position++;
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
 
-if (position >= bufsize)
-{
-bufsize += TOKEN_BUFSIZE;
-tokens = realloc(tokens, bufsize *sizeof(char *));
-if (tokens == NULL)
-{
-perror("allocation error");
-exit(EXIT_FAILURE);
-}
-}
+        if (position >= bufsize) {
+            bufsize += TOKEN_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (tokens == NULL) {
+                perror("allocation error");
+                exit(EXIT_FAILURE);
+            }
+        }
 
-token = strtok(NULL, delimiters);
-}
-tokens[position] = NULL;
-return (tokens);
+        token = strtok(NULL, delimiters);
+    }
+    tokens[position] = NULL;
+    return tokens;
 }
 
