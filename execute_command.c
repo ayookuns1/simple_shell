4dffa1630fe3cd230b2b68a518f4a4e9c1ec16c6
ayookuns1 <ayookuns830@gmail.com>
2023-08-23 09:44:25 -0700

    omoo

--- execute_command.c
+++ execute_command.c
@@ -1,25 +1,12 @@
 #include "simple_shell.h"
-/**
- * execute_command_with_path - This Fuctn execute a comnd
- * using its full path
- * @args: Arr of strs containing the comnd and its args.
- * Fuctn attempts to execute the specified command using its full path
- *
- * Return: 1 (On success), 0 (command not found)
- */
+
 int execute_command_with_path(char **args) {
     pid_t pid;
     int status;
 
     char *command_path = find_command_in_path(args[0]);
     if (command_path == NULL) {
-        return 0; 
-    }
-
-    if (access(command_path, X_OK) == -1) {
-        perror("simple_shell");
-        free(command_path);
-        return 0;
+        return (0);
     }
 
     pid = fork();
@@ -35,6 +22,6 @@ int execute_command_with_path(char **args) {
     }
 
     free(command_path);
-    return 1;
+    return (1);
 }
 
--- main.c
+++ main.c
@@ -1,23 +1,15 @@
 #include "simple_shell.h"
-/**
- * main - This is entry point of simple shell program
- *
- * Fuctn implemnts the main loop of the shell
- *
- * Return: EXIT_SUCCESS (Always)
- */
-int main(void)
-{
-int interactive = isatty(STDIN_FILENO);
 
-if (!interactive)
-{
-handle_non_interactive();
-return (EXIT_SUCCESS);
-}
+int main(void) {
+    int interactive = isatty(STDIN_FILENO);
 
-handle_interactive();
+    if (!interactive) {
+        handle_non_interactive();
+        return (EXIT_SUCCESS);
+    }
 
-return (EXIT_SUCCESS);
+    handle_interactive();
+
+    return (EXIT_SUCCESS);
 }
 
