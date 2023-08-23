2f1b7d9e9f8d170bbe61fc600572970e2a1ef911
ayookuns1 <ayookuns830@gmail.com>
2023-08-23 01:07:57 -0700

    update1

--- main.c
+++ main.c
@@ -10,8 +10,36 @@ int main(void)
 {
 char *input;
 char **args;
+int interactive = isatty(STDIN_FILENO);
 
-while (1)
+if (!interactive)
+{
+size_t input_size = 0;
+
+ssize_t read = getline(&input, &input_size, stdin);
+if (read > 0 && input[read - 1] == '\n')
+input[read - 1] = '\0';
+
+args = parse_input(input);
+
+if (args[0] != NULL)
+{
+if (execute_builtin(args) == 0)
+{
+if (!execute_command_with_path(args))
+{
+fprintf(stderr, "Command not found: %s\n", args[0]);
+exit(EXIT_FAILURE);
+}
+}
+}
+
+
+return (EXIT_SUCCESS);
+}
+
+
+while (interactive)
 {
 printf("shell$ ");
 fflush(stdout);
@@ -28,7 +56,14 @@ printf("Command not found: %s\n", args[0]);
 }
 }
 }
+
+
 }
 return (EXIT_SUCCESS);
 }
+
+
+
+
+
 
